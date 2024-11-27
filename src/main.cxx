0cc6d9d5fdd590a669c0b55684afcf0c6f906612
Jimmy Downer <jamesdanieldowner@gmail.com>
2024-11-27 00:38:45 -0700

    Debugs

--- src/main.cxx
+++ src/main.cxx
@@ -1,3 +1,4 @@
+#include <cstdlib>
 #include <iostream>
 
 #define GLAD_GL_IMPLEMENTATION
@@ -8,21 +9,30 @@
 
 #define QUOTE(x) #x
 #define STRING(x) QUOTE(x)
-
 #ifdef DEBUG
-void errorCallbackGLFW(int /*error*/, const char* description) {
-  std::cerr << "GLFW error: " << description << std::endl;
-}
+#define DEBUG_LOG(s) do { std::cout << s; } while (false);
+#define DEBUG_ERROR(s) do { std::cerr << s; } while (false);
+#define DEBUG_LOG_LINE(s) do { std::cout << s << std::endl; } while (false);
+#define DEBUG_ERROR_LINE(s) do { std::cerr << s << std::endl; } while (false);
+#else
+#define DEBUG_LOG(s)
+#define DEBUG_ERROR(s)
+#define DEBUG_LOG_LINE(s)
+#define DEBUG_ERROR_LINE(s)
 #endif
 
+void errorCallbackGLFW(int /*error*/, const char* description) {
+  DEBUG_ERROR_LINE("GLFW error: " << description);
+}
+
 void keyCallback(GLFWwindow *window, int key, int /*scancode*/, int action, int mods) {
-  // int keyDown{action & GLFW_KEY_DOWN};
-  int keyUp{action & GLFW_KEY_UP};
-  bool keyQ{key == GLFW_KEY_Q};
-  bool keyW{key == GLFW_KEY_W};
-  bool keyF4{key == GLFW_KEY_F4};
-  int keyCtrl{mods & GLFW_MOD_CONTROL};
-  int keyAlt{mods & GLFW_MOD_ALT};
+  // const int keyDown{action & GLFW_KEY_DOWN};
+  const int keyUp{action & GLFW_KEY_UP};
+  const bool keyQ{key == GLFW_KEY_Q};
+  const bool keyW{key == GLFW_KEY_W};
+  const bool keyF4{key == GLFW_KEY_F4};
+  const int keyCtrl{mods & GLFW_MOD_CONTROL};
+  const int keyAlt{mods & GLFW_MOD_ALT};
   if (keyUp && ((keyCtrl && (keyQ || keyW)) || (keyAlt && keyF4))) {
     glfwSetWindowShouldClose(window, GLFW_TRUE);
     return;
@@ -31,8 +41,8 @@ void keyCallback(GLFWwindow *window, int key, int /*scancode*/, int action, int 
 
 int main(int, char**) {
   if (!glfwInit()) {
-    std::cerr << "GLFW error: Initialization failed" << std::endl;
-    std::exit(1);
+    DEBUG_ERROR_LINE("GLFW error: Initialization failed");
+    std::exit(EXIT_FAILURE);
   }
 #ifdef DEBUG
   glfwSetErrorCallback(errorCallbackGLFW);
@@ -53,17 +63,21 @@ int main(int, char**) {
 #ifdef __APPLE__
   glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-  GLFWwindow* window{glfwCreateWindow(windowWidth, windowHeight, windowTitle, windowMonitor, windowShare)};
+  GLFWwindow* window{glfwCreateWindow(
+    windowWidth,
+    windowHeight,
+    windowTitle,
+    windowMonitor,
+    windowShare
+  )};
   if (!window) {
     glfwTerminate();
-    std::exit(1);
+    std::exit(EXIT_FAILURE);
   }
   glfwMakeContextCurrent(window);
   gladLoadGL(glfwGetProcAddress);
-#ifdef DEBUG
-  std::cout << "C++ version: " << STRING(__cplusplus) << std::endl;
-  std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
-#endif
+  DEBUG_LOG_LINE("C++ version: " << STRING(__cplusplus));
+  DEBUG_LOG_LINE("OpenGL version: " << glGetString(GL_VERSION));
   glfwSwapInterval(1);
   glfwSetKeyCallback(window, keyCallback);
   while (!glfwWindowShouldClose(window)) {
@@ -75,7 +89,6 @@ int main(int, char**) {
     glDepthFunc(GL_LEQUAL);
     glClearColor(0., .5, 1., 1.);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
